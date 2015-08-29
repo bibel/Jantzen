e0b3fb8a602ba490fdafe2d4575d2c97f239e90e
Bibelsammler <bibelsammler@uni.de>
2015-08-29 23:15:17 +0200

    first commit

---
+++ metadata.js
@@ -0,0 +1,226 @@
+biblename = "Das Neue Testament in deutscher Fassung und die Psalmen [Herbert Jantzen] (2011) [www.sermon-online.de]";
+metadata = [{
+abbr:"Mt",
+short:"Die gute Botschaft nach Matthäus",
+long:"Die gute Botschaft nach Matthäus",
+osis:"Matt",
+type:"nt",
+nt:true,
+chapters:28
+},{
+abbr:"Mk",
+short:"Die gute Botschaft nach Markus",
+long:"Die gute Botschaft nach Markus",
+osis:"Mark",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"Lk",
+short:"Die gute Botschaft nach Lukas",
+long:"Die gute Botschaft nach Lukas",
+osis:"Luke",
+type:"nt",
+nt:true,
+chapters:24
+},{
+abbr:"Jh",
+short:"Die gute Botschaft nach Johannes",
+long:"Die gute Botschaft nach Johannes",
+osis:"John",
+type:"nt",
+nt:true,
+chapters:21
+},{
+abbr:"Ag",
+short:"Die Apostelgeschichte",
+long:"Die Apostelgeschichte",
+osis:"Acts",
+type:"nt",
+nt:true,
+chapters:28
+},{
+abbr:"Rm",
+short:"Der Römerbrief",
+long:"Der Römerbrief",
+osis:"Rom",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"1Kr",
+short:"Der 1. Korintherbrief",
+long:"Der 1. Korintherbrief",
+osis:"1Cor",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"2Kr",
+short:"Der 2. Korintherbrief",
+long:"Der 2. Korintherbrief",
+osis:"2Cor",
+type:"nt",
+nt:true,
+chapters:13
+},{
+abbr:"Ga",
+short:"Der Galaterbrief",
+long:"Der Galaterbrief",
+osis:"Gal",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"Eph",
+short:"Der Epheserbrief",
+long:"Der Epheserbrief",
+osis:"Eph",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"Php",
+short:"Der Philipperbrief",
+long:"Der Philipperbrief",
+osis:"Phil",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"Kol",
+short:"Der Kolosserbrief",
+long:"Der Kolosserbrief",
+osis:"Col",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"1Th",
+short:"Der 1. Thessalonicherbrief",
+long:"Der 1. Thessalonicherbrief",
+osis:"1Thess",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2Th",
+short:"Der 2. Thessalonicherbrief",
+long:"Der 2. Thessalonicherbrief",
+osis:"2Thess",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"1Tm",
+short:"Der 1. Timotheusbrief",
+long:"Der 1. Timotheusbrief",
+osis:"1Tim",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"2Tm",
+short:"Der 2. Timotheusbrief",
+long:"Der 2. Timotheusbrief",
+osis:"2Tim",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"Tt",
+short:"Der Titusbrief",
+long:"Der Titusbrief",
+osis:"Titus",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"Phm",
+short:"Der Philemonbrief",
+long:"Der Philemonbrief",
+osis:"Phlm",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Heb",
+short:"Der Hebräerbrief",
+long:"Der Hebräerbrief",
+osis:"Heb",
+type:"nt",
+nt:true,
+chapters:13
+},{
+abbr:"Jk",
+short:"Der Jakobusbrief",
+long:"Der Jakobusbrief",
+osis:"Jas",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"1P",
+short:"Der 1. Petrusbrief",
+long:"Der 1. Petrusbrief",
+osis:"1Pet",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2P",
+short:"Der 2. Petrusbrief",
+long:"Der 2. Petrusbrief",
+osis:"2Pet",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"1Jh",
+short:"Der 1. Johannesbrief",
+long:"Der 1. Johannesbrief",
+osis:"1John",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2Jh",
+short:"Der 2. Johannesbrief",
+long:"Der 2. Johannesbrief",
+osis:"2John",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"3Jh",
+short:"Der 3. Johannesbrief",
+long:"Der 3. Johannesbrief",
+osis:"3John",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Jud",
+short:"Der Judasbrief",
+long:"Der Judasbrief",
+osis:"Jude",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Off",
+short:"Die Offenbarung",
+long:"Die Offenbarung",
+osis:"Rev",
+type:"nt",
+nt:true,
+chapters:22
+},{
+abbr:"Ps",
+short:"Die Psalmen",
+long:"Die Psalmen",
+osis:"Ps",
+type:"ot",
+nt:false,
+chapters:150
+}];
